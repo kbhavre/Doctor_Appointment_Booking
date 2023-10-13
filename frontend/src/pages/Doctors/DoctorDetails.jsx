feab79f26ca94a720b1e3292e4394ff6324c5e33
kunal <kbhavre78@gmail.com>
2023-10-13 15:08:56 +0530

    Completed upto Login page

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -3,6 +3,7 @@ import doctorImg from '../../assets/images/doctor-img02.png'
 import starIcon from '../../assets/images/Star.png'
 import DoctorAbout from './DoctorAbout'
 import Feedback from './Feedback'
+import SidePanel from './SidePanel'
 
 const DoctorDetails = () => {
 
@@ -54,14 +55,13 @@ const DoctorDetails = () => {
             </div>
 
             <div className="mt-[50px]">
-              {
-                tab==='about' && <DoctorAbout/>
-              }
-              {
-                tab==='feedback' && <Feedback/>
-              }
+              {tab === 'about' && <DoctorAbout />}
+              {tab === 'feedback' && <Feedback />}
+            </div>
             </div>
 
+          <div>
+            <SidePanel/>
           </div>
 
         </div>
