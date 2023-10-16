0ab2dbb6665b207ad356a9b05ca44b145f2ea7ef
kunal <kbhavre78@gmail.com>
2023-10-16 15:56:28 +0530

    Fix: service section issues

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import ServiceList from '../components/Services/ServiceList'
+import {services} from '../assets/data/services';
+import ServiceCard from '../components/Services/ServiceCard';
 
-export default function Services() {
+const Services = () => {
   return (
-    <div>Services</div>
+    <section>
+        <div className="container">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px]">
+            {services.map((item, index)=>(
+              <ServiceCard item={item} index={index} key={index}/>
+            ))}
+          </div>
+        </div>
+    </section>
   )
 }
+
+export default Services
