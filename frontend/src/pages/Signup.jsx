c5ce1f24d6aac219dc7da47e232406431d71f7ee
kunal <kbhavre78@gmail.com>
2024-03-29 23:15:02 +0530

    Completed upto cloudinary api

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,35 +1,72 @@
 import React, { useState } from 'react'
 import signupImg from '../assets/images/signup.gif'
 import avatar from '../assets/images/doctor-img01.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import uploadImageToCloudinary from '../utils/uploadCloudinary'
+import { BASE_URL } from '../config'
+import { toast } from 'react-toastify'
+import HashLoader from 'react-spinners/HashLoader'
 
 const Signup = () => {
 
   const [selectedFile, setSelectedFile] = useState(null);
   const [previewURL, setPreviewURL] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const [formData, setFormData] = useState({
-    name:'',
+    name: '',
     email: '',
     password: '',
-    photo:'',
-    gender:'',
-    role:'patient'
-  })
+    photo: '',
+    gender: '',
+    role: 'patient'
+  });
+
+  const navigate = useNavigate();
 
   const handleInputChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
   }
 
-  const handleFileInputChange = async (event)=>{
+  const handleFileInputChange = async (event) => {
 
     const file = event.target.files[0];
 
+    const data = await uploadImageToCloudinary(file);
+
+    setPreviewURL(data.url)
+    setSelectedFile(file.url)
+    setFormData({ ...formData, photo: data.url });
     // Baad me hum yaha cloudinary use karenge images upload karne ke liye
   }
 
-  const submitHandler = async event=>{
-    event.preventDefault()
+  const submitHandler = async event => {
+
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/register`, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      const { message } = await res.json()
+
+      if (!res.ok) {
+        throw new Error(message)
+      }
+      setLoading(false)
+      toast.success(message)
+      navigate('/login')   //agar signup success hua toh hame login par redirect karega
+
+    }
+    catch (err) {
+      toast.error(err.message)
+      setLoading(false)
+    }
   }
   return (
     <section className='px-5 xl:px-0'>
@@ -85,11 +122,11 @@ const Signup = () => {
 
                 <label className='text-headingColor font-bold text-[16px] leading-7'>
                   Are you a:
-                  <select 
-                  name="role"
-                  value={formData.role}
-                  onChange={handleInputChange}
-                  className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none' id="">
+                  <select
+                    name="role"
+                    value={formData.role}
+                    onChange={handleInputChange}
+                    className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none' id="">
                     <option value="patient">Patient</option>
                     <option value="Doctor">Doctor</option>
                   </select>
@@ -97,11 +134,11 @@ const Signup = () => {
 
                 <label className='text-headingColor font-bold text-[16px] leading-7'>
                   Gender:
-                  <select 
-                  name="gender" 
-                  value={formData.gender}
-                  onChange={handleInputChange}
-                  className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none' id="">
+                  <select
+                    name="gender"
+                    value={formData.gender}
+                    onChange={handleInputChange}
+                    className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none' id="">
                     <option value="">Select</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
@@ -111,9 +148,9 @@ const Signup = () => {
               </div>
 
               <div className="mb-5 flex items-center gap-3 ">
-                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
-                  <img src={avatar} alt="" className='w-full rounded-full' />
-                </figure>
+                {selectedFile && <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+                  <img src={previewURL} alt="" className='w-full rounded-full' />
+                </figure>}
 
                 <div className='relative w-[130px] h-[50px] '>
                   <input
@@ -132,17 +169,17 @@ const Signup = () => {
               </div>
 
               <div className="mt-7">
-            <button type="submit" className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3 " >
-              Sign Up
-            </button>
-          </div>
+                <button disabled={loading && true} type="submit" className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3 " >
+                  {loading ? <HashLoader size={35} color="#fffff" /> : "Sign Up"}
+                </button>
+              </div>
 
-          <p className="mt-5 text-textColor text-center">
-            Already have an account?
-            <Link to='/login' className="text-primaryColor font-medium ">
-              Login
-            </Link>
-          </p>
+              <p className="mt-5 text-textColor text-center">
+                Already have an account?
+                <Link to='/login' className="text-primaryColor font-medium ">
+                  Login
+                </Link>
+              </p>
 
             </form>
           </div>
